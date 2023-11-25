1565205ab16edcee4d55a485b3f0c7fb1bc317c2
mafterr11 <mafterr11@gmail.com>
2023-11-25 23:05:28 +0200

    Commit

--- app/components/TestimonialSlider.jsx
+++ app/components/TestimonialSlider.jsx
@@ -1,30 +1,75 @@
+"use client"
 // testimonial data
 const testimonialData = [
   {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
+    image: "/t-avt-1.png",
+    name: "Anne Smith",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
+    image: "/t-avt-2.png",
+    name: "Jane Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
+    image: "/t-avt-3.png",
+    name: "Jhon Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
 ];
 
+// Swiper Components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Swiper Styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// Icons
+import { BsArrowRight } from "react-icons/bs";
+
+// Required Modules
+import { Pagination } from "swiper";
+import Image from "next/image";
+
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className='h-[280px] sm:h-[340px]'
+    >
+      {testimonialData.map((person, index) => {
+        return <SwiperSlide key={index}>
+          <div>
+            {/* avatar, name position  */}
+            <div>
+              <div>
+                {/* avatar */}
+                <div>avatar image</div>
+                {/* name */}
+                <div>name</div>
+                {/* position */}
+                <div>position</div>
+              </div>
+            </div>
+            {/* quote and message */}
+            <div>quote and message</div>
+          </div>
+        </SwiperSlide>;
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
--- app/components/WorkSlider.jsx
+++ app/components/WorkSlider.jsx
@@ -75,8 +75,8 @@ const WorkSlider = () => {
             <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
               {slide.images.map((image, index) => {
                 return (
-                  <div className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
-                    <div className='flex items-center justify-center relative overflow-hidden'>
+                  <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={index}>
+                    <div className='flex items-center justify-center relative overflow-hidden group'>
                       {/* image */}
                       <Image
                         src={image.path}
@@ -85,18 +85,22 @@ const WorkSlider = () => {
                         alt='my work images'
                       />
                       {/* overlay gradient */}
-                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-inherit via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
+                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
                       {/* title */}
-                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 xl:group-hover:-translate-y-20">
-                        <div>
+                      <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20'>
+                        <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                           {/* title part 1 */}
-                          <div>title part 1</div>
-                         {/* title part 2 */}
-                          <div>title part 2</div>
-                         {/* icon*/}
-                          <div>icon</div>
-                        
-                          
+                          <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+                            LIVE
+                          </div>
+                          {/* title part 2 */}
+                          <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+                            PROJECT
+                          </div>
+                          {/* icon*/}
+                          <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300'>
+                            <BsArrowRight />
+                          </div>
                         </div>
                       </div>
                     </div>
