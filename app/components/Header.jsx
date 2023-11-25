f7238a41aa951d380dcb0fb30e250bf582d258f5
mafterr11 <mafterr11@gmail.com>
2023-11-25 16:49:52 +0200

    Commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className='absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px] '>
       <div className='container mx-auto'>
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-6">
           {/* logo */}
           <Link href={'/'}><Image src={Logo} width={220} height={48} alt="header logo" priority={true}></Image>
           </Link>
