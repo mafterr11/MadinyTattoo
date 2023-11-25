cd9a88ef75215d3e936ab1b472d71b8a90213160
mafterr11 <mafterr11@gmail.com>
2023-11-25 15:28:39 +0200

    Commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -105,12 +105,23 @@ const About = () => {
     <Avatar />
     </motion.div>
     <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-      <div>text</div>
-      <div>
+      <div className="flex-1 flex flex-col justify-center">text</div>
+      <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
         <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
           {aboutData.map((item, itemIndex)=>{
             return <div key={itemIndex} className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'}
-            cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}>{item.title}</div>
+            cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`} 
+            onClick={()=>setIndex(itemIndex)}>{item.title}</div>
+          })}
+        </div>
+        <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+          {aboutData[index].info.map((item, itemIndex)=>{
+            return <div key={itemIndex}>
+              {/* title */}
+              <div>{item.title}</div>
+              <div className="hidden md:flex">-</div>
+              <div>{item.stage}</div>
+            </div>
           })}
         </div>
       </div>
