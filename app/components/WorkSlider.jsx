c1e789cf5a4b871e103ccf95e15f0a54a8f620a9
mafterr11 <mafterr11@gmail.com>
2023-11-25 22:44:58 +0200

    Commit

--- app/components/WorkSlider.jsx
+++ app/components/WorkSlider.jsx
@@ -1,51 +1,114 @@
-// data
+// Data
 const workSlides = {
   slides: [
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
       ],
     },
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
       ],
     },
   ],
 };
 
+// Swiper Components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Swiper Styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// Icons
+import { BsArrowRight } from "react-icons/bs";
+
+// Required Modules
+import { Pagination } from "swiper";
+import Image from "next/image";
+
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className='h-[280px] sm:h-[340px]'
+    >
+      {workSlides.slides.map((slide, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+              {slide.images.map((image, index) => {
+                return (
+                  <div className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+                    <div className='flex items-center justify-center relative overflow-hidden'>
+                      {/* image */}
+                      <Image
+                        src={image.path}
+                        width={500}
+                        height={300}
+                        alt='my work images'
+                      />
+                      {/* overlay gradient */}
+                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-inherit via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
+                      {/* title */}
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 xl:group-hover:-translate-y-20">
+                        <div>
+                          {/* title part 1 */}
+                          <div>title part 1</div>
+                         {/* title part 2 */}
+                          <div>title part 2</div>
+                         {/* icon*/}
+                          <div>icon</div>
+                        
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
