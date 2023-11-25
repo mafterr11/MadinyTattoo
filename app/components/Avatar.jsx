e0e105635dc0d9a2ca229602bcb5c43f4e4fd882
mafterr11 <mafterr11@gmail.com>
2023-11-25 14:26:34 +0200

    Animation Complete

--- app/components/Avatar.jsx
+++ app/components/Avatar.jsx
@@ -1,5 +1,10 @@
+import Image from "next/image";
+import avatar from "../../public/Avatar.png"
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return <div className="hidden xl:flex xl:max-w-none">
+    <Image src={avatar} width={737} height={678}/>
+  </div>;
 };
 
 export default Avatar;
