317f065ce94e7c662114888abbc15e33ad1d9cc1
mafterr11 <mafterr11@gmail.com>
2023-11-25 12:03:05 +0200

    Commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+// COMPONENTS
+import ParticleContainer from './components/ParticlesContainer'
+import ProjectsBtn from './components/ProjectsBtn'
+import Avatar from './components/Avatar'
+
+// FRAMER MOTION
+import { motion } from "framer-motion";
+
+// VARIANTS
+import {fadeIn} from '../variants'
+
 const Home = () => {
-    return <div className="">Home</div>;
+    return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <h1 className="h1">Transforming Ideas <br /> Into <span className="text-accent">Digital Reality</span>
+          </h1>
+          {/* subtitle */}
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo libero nihil ratione ut numquam illo incidunt repellendus.</p>
+        </div>
+      </div>
+      {/* image */}
+      <div>image</div>
+      </div>
+    )
   };
   
   export default Home;
