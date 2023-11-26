7474bcbb3d704e4de9e6c470302c0da474dc1d0d
mafterr11 <mafterr11@gmail.com>
2023-11-26 12:50:57 +0200

    Transition Color

--- app/components/Transition.jsx
+++ app/components/Transition.jsx
@@ -21,7 +21,7 @@ const Transition = () => {
   return (
     <>
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#A4907C]'
         variants={transitionVariants}
         initial='initial'
         animate='animate'
@@ -30,7 +30,7 @@ const Transition = () => {
       ></motion.div>
 
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#C8B6A6]'
         variants={transitionVariants}
         initial='initial'
         animate='animate'
@@ -39,7 +39,7 @@ const Transition = () => {
       ></motion.div>
 
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#F1DEC9]'
         variants={transitionVariants}
         initial='initial'
         animate='animate'
