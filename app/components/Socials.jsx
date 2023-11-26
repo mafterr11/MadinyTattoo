ae2abf493f795606eb38b0b2cd1b61c65c9455b4
mafterr11 <mafterr11@gmail.com>
2023-11-26 14:17:16 +0200

    Commit

--- app/components/Socials.jsx
+++ app/components/Socials.jsx
@@ -1,29 +1,45 @@
 import Link from "next/link";
-import {
-  RiYoutubeLine,
-  RiInstagramLine,
-  RiFacebookLine,
-  RiDribbbleLine,
-  RiBehanceLine,
-  RiPinterestLine,
-} from "react-icons/ri";
+import { RiInstagramLine, RiTiktokLine, RiWhatsappLine } from "react-icons/ri";
+import { BiPhone } from "react-icons/bi";
+
+
 
 const Socials = () => {
-  return <div className="flex items-center gap-x-5 text-2xl">
-    <Link href={''} className="hover:text-accent transition-all duration-300"><RiYoutubeLine/></Link>
+  return (
+    <div className='flex items-center gap-x-5 text-3xl'>
+      <Link
+        href={"https://www.instagram.com/madinytattoo/"}
+        target='blank'
+        className='hover:text-accent transition-all duration-300'
+      >
+        <RiInstagramLine />
+      </Link>
 
-    <Link href={'https://www.instagram.com/madinytattoo/'} target="blank" className="hover:text-accent transition-all duration-300"><RiInstagramLine/></Link>
+      <Link
+        href={"https://www.tiktok.com/@madiny.tattoo"}
+        target='blank'
+        className='hover:text-accent transition-all duration-300'
+      >
+        <RiTiktokLine />
+      </Link>
 
-    <Link href={''} className="hover:text-accent transition-all duration-300"><RiFacebookLine/></Link>
+      <a
+        href='tel:+40741620774'
+        target='blank'
+        className='hover:text-accent transition-all duration-300'
+      >
+        <BiPhone />
+      </a>
 
-    <Link href={''} className="hover:text-accent transition-all duration-300"><RiDribbbleLine/></Link>
-
-    <Link href={''} className="hover:text-accent transition-all duration-300"><RiBehanceLine/></Link>
-
-    <Link href={''} className="hover:text-accent transition-all duration-300"><RiPinterestLine/></Link>
-
-    
-  </div>;
+      <a
+        href='http://wa.me/+40741620774'
+        target='blank'
+        className='hover:text-accent transition-all duration-300'
+      >
+        <RiWhatsappLine />
+      </a>
+    </div>
+  );
 };
 
 export default Socials;
