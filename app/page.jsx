fe1c45722f1d41502d0c697f234d047bd4e4aa37
Alexandru Maftei <mafterr11@gmail.com>
2023-11-27 14:10:05 +0200

    HomePage Responsive

--- app/page.jsx
+++ app/page.jsx
@@ -14,8 +14,8 @@ const Home = () => {
   return (
     <div className='bg-primary/60 h-full'>
       {/* text */}
-      <div className='w-full h-full bg-gradient-to-r pt-24'>
-        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+      <div className='w-full h-full bg-gradient-to-r py-12'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-screen xl:h-full container mx-auto'>
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
