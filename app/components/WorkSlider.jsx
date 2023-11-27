cfaa3dd678783dfae390e5c029f600d06ac005de
mafterr11 <mafterr11@gmail.com>
2023-11-27 20:48:40 +0200

    Commit

--- app/components/WorkSlider.jsx
+++ app/components/WorkSlider.jsx
@@ -76,31 +76,30 @@ import "swiper/css/pagination";
 import { BsArrowRight } from "react-icons/bs";
 
 // Required Modules
-import { Navigation, Pagination } from "swiper";
+import { Pagination } from "swiper";
 import Image from "next/image";
 
 const WorkSlider = () => {
   return (
     <Swiper
       spaceBetween={10}
-      navigation={true}
       pagination={{
         clickable: true,
       }}
-      modules={[Navigation, Pagination]}
+      modules={[Pagination]}
       className='h-fit'
     >
       {workSlides.slides.map((slide, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className='grid grid-cols-2 grid-rows-2 gap-2 cursor-pointer'>
+            <div className='grid grid-cols-2 grid-rows-2 gap-2 sm:gap-4 cursor-pointer'>
               {slide.images.map((image, index) => {
                 return (
                   <div
                     className='relative rounded-lg overflow-hidden flex items-center justify-center group'
                     key={index}
                   >
-                    <div className='flex items-center justify-center relative overflow-hidden group border-black/40 border-solid border-2'>
+                    <div className='flex items-center justify-center relative overflow-hidden group'>
                       {/* image */}
                       <Image
                         src={image.path}
@@ -109,22 +108,18 @@ const WorkSlider = () => {
                         alt='my work images'
                       />
                       {/* overlay gradient */}
-                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
+                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#50484f] to-[#2f2942] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
                       {/* title */}
                       <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20'>
                         <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
-                          {/* title part 1 */}
-                          <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
-                            LIVE
-                          </div>
-                          {/* title part 2 */}
-                          <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
-                            PROJECT
-                          </div>
+                          {/* title  */}
+                          {/* <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+                            LIVE PROJECT
+                          </div> */}
                           {/* icon*/}
-                          <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300'>
+                          {/* <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300'>
                             <BsArrowRight />
-                          </div>
+                          </div> */}
                         </div>
                       </div>
                     </div>
