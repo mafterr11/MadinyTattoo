3d4da27cde05f7eea2f5144378020464baf1c960
Alexandru Maftei <mafterr11@gmail.com>
2023-11-29 14:23:31 +0200

    Commit

--- app/components/WorkSlider.jsx
+++ app/components/WorkSlider.jsx
@@ -108,7 +108,7 @@ const WorkSlider = () => {
                         alt='my work images'
                       />
                       {/* overlay gradient */}
-                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#50484f] to-[#2f2942] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
+                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#d4b39a54] to-[#d4b39a56] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
                       {/* title */}
                       <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20'>
                         <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
