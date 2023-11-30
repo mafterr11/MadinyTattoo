ad6e6366c2c4140611d0be2344d7f992f6386d04
mafterr11 <mafterr11@gmail.com>
2023-11-30 11:54:35 +0200

    Commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
 import HomePage from "../components/HomePage";
 
+export const metadata = {
+  description:
+    "Bine ați venit la Madiny Tattoo, locul unde arta devine permanentă. Descoperă cele mai recente creații în tatuaje, micropigmentare și eliminare eficientă a tatuajelor. Expertiză, pasiune și creație autentică.",
+};
 
 const Acasa = () => {
   return <HomePage />;
