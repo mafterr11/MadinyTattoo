e890efde3653b6bb6e956b36392aa435f11f6269
mafterr11 <mafterr11@gmail.com>
2023-12-02 20:45:29 +0200

    Conmmit

--- components/ContactPage.jsx
+++ components/ContactPage.jsx
@@ -13,85 +13,86 @@ import { fadeIn } from "../variants";
 
 const ContactPage = () => {
   return (
-    <section class=" body-font relative h-full pt-44">
+    <section class=' body-font relative h-full lg:pt-44 pt-32'>
       <motion.h2
         variants={fadeIn("right", 0, 2)}
-        initial="hidden"
-        animate="show"
-        exit="hidden"
-        className="h2 text-center mb-12"
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className='h2 text-center lg:mb-12'
       >
-        Să ne <span className="text-accent">conectăm.</span>
+        Să ne <span className='text-accent'>conectăm.</span>
       </motion.h2>
       <motion.div
         variants={fadeIn("left", 0, 4)}
-        initial="hidden"
-        animate="show"
-        exit="hidden"
-        class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap h-[800px]"
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        class='container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap h-[800px]'
       >
-        <div class="lg:w-full md:w-1/2 bg-gray-300 rounded-lg overflow-hidden p-10 flex items-end justify-start relative">
+        <div class='lg:w-full md:w-1/2 bg-gray-300 rounded-lg overflow-hidden p-10 flex flex-col lg:flex-row lg:items-end lg:justify-start justify-end relative'>
           <iframe
-            width="100%"
-            height="100%"
-            className="absolute inset-0"
-            frameBorder="0"
-            title="map"
-            marginHeight="0"
-            marginWidth="0"
-            scrolling="no"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11385.737698530624!2d26.1075064!3d44.4857646!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b203567ff26e41%3A0x3c22c06af60c1061!2sMadiny%20Tattoo!5e0!3m2!1sen!2sro!4v1701444628772!5m2!1sen!2sro"
+            width='100%'
+            height='100%'
+            className='absolute inset-0'
+            frameBorder='0'
+            title='map'
+            marginHeight='0'
+            marginWidth='0'
+            scrolling='no'
+            src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11385.737698530624!2d26.1075064!3d44.4857646!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b203567ff26e41%3A0x3c22c06af60c1061!2sMadiny%20Tattoo!5e0!3m2!1sen!2sro!4v1701444628772!5m2!1sen!2sro'
             style={{ filter: "grayscale(0.4) contrast(1) opacity(0.7)" }}
           ></iframe>
 
-          <div class="bg-white relative flex flex-wrap py-4 rounded shadow-md items-start justify-end">
-            <div class="lg:w-1/2 px-4">
-              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-sm">
+          <div class='bg-white/90 lg:bg-white relative flex flex-wrap py-4 mb-4 rounded shadow-md items-start justify-end'>
+            <div class='lg:w-1/2 px-4'>
+              <h2 class='title-font font-semibold text-gray-900 tracking-widest text-sm'>
                 ADDRESS
               </h2>
-              <p class="mt-1 text-accent font-bold leading-relaxed">
+              <p class='mt-1 text-accent font-bold leading-relaxed'>
                 Șos. Pipera 61, București 077190
               </p>
             </div>
-            <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-sm">
+            <div class='lg:w-1/2 px-6 mt-4 lg:mt-0'>
+              <h2 class='title-font font-semibold text-gray-900 tracking-widest text-sm'>
                 EMAIL
               </h2>
               <a
-                href="mailto: contact@madinytattoo.ro"
-                className="text-accent hover:underline hover:underline-offset-8 transition-all duration-300 leading-relaxed font-bold"
+                href='mailto: contact@madinytattoo.ro'
+                className='text-accent hover:underline hover:underline-offset-8 transition-all duration-300 leading-relaxed font-bold'
               >
                 contact@madinytattoo.ro
               </a>
-              <h2 class="font-semibold text-gray-900 tracking-widest mt-4 text-sm">
+              <h2 class='font-semibold text-gray-900 tracking-widest mt-4 text-sm'>
                 PHONE
               </h2>
               <a
-                href="tel:+40741620774"
-                target="blank"
-                className="text-accent hover:underline hover:underline-offset-8 transition-all duration-300  leading-relaxed font-bold"
+                href='tel:+40741620774'
+                target='blank'
+                className='text-accent hover:underline hover:underline-offset-8 transition-all duration-300  leading-relaxed font-bold'
               >
                 +04.741.620.774
               </a>
             </div>
           </div>
           {/* form */}
-          <div class="lg:w-1/3 md:w-1/2 bg-white z-10 flex flex-col md:ml-auto w-full md:p-6 mt-8 md:mt-0 mr-4">
-            <h2 class="font-semibold text-gray-900 tracking-widest mb-1">
+          <div class='lg:w-1/3 md:w-1/2 bg-white/90 lg:bg-white z-10 flex flex-col md:ml-auto w-full p-4 mt-8 md:mt-0 mr-4 lg:mb-[22rem] rounded shadow-md'>
+            <h2 class='font-semibold text-gray-900 tracking-widest mb-1'>
               Program:
             </h2>
 
-            <div class="relative mb-4">
+            <div class='relative mb-4'>
               <p
-                for="name"
-                class="leading-relaxed text-gray-900 font-semibold "
+                for='name'
+                class='leading-relaxed text-gray-900 font-semibold flex flex-col lg:flex-row gap-x-1'
               >
-               <span className="text-accent">Luni-Sambata:</span>  11:00 am - 22:00 pm
+                <span className='text-accent'>Luni-Sambata:</span> 11:00 am -
+                22:00 pm
               </p>
             </div>
-            <div class="relative">
-              <p for="name" class="leading-relaxed text-gray-900 font-semibold">
-              <span className="text-accent">Duminica:</span> Inchis
+            <div class='relative'>
+              <p for='name' class='leading-relaxed text-gray-900 font-semibold'>
+                <span className='text-accent'>Duminica:</span> Inchis
               </p>
             </div>
           </div>
