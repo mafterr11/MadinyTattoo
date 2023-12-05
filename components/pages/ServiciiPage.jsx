f246f7833532f3c2f0265af66d0e23cbf7ae2074
Alexandru Maftei <mafterr11@gmail.com>
2023-12-05 13:53:24 +0200

    Commit

--- components/pages/ServiciiPage.jsx
+++ components/pages/ServiciiPage.jsx
@@ -5,7 +5,7 @@ import { fadeIn } from "../../variants";
 
 const Services = () => {
   return (
-    <div className='md:h-screen py-36 flex items-center'>
+    <div className='md:h-screen py-36 flex items-center lg:pr-24'>
       {/* <Circles /> */}
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
