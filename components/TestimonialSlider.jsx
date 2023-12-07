c74d028d13442bdc0ce9b9b6273930c984d83c01
Alexandru Maftei <mafterr11@gmail.com>
2023-12-07 12:55:58 +0200

    Testimoniale

--- components/TestimonialSlider.jsx
+++ components/TestimonialSlider.jsx
@@ -34,6 +34,8 @@ import "swiper/css/pagination";
 
 // Icons
 import { FaQuoteLeft } from "react-icons/fa";
+import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
+
 
 // Required Modules
 import { Navigation, Pagination } from "swiper";
@@ -41,47 +43,56 @@ import Image from "next/image";
 
 const TestimonialSlider = () => {
   return (
-    <Swiper
-      navigation={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Navigation, Pagination]}
-      className='h-[500px]'
-    >
-      {testimonialData.map((person, index) => {
-        return (
-          <SwiperSlide key={index}>
-            <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
-              <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
-                <div className='flex flex-col justify-center text-center'>
-                  <div className='mb-2 mx-auto'>
-                    <Image
-                      src={person.image}
-                      width={100}
-                      height={100}
-                      alt='testimonial image'
-                    />
+    <div className="relative">
+      <Swiper
+        navigation={{
+          nextEl: ".swiper-navigation-next",
+          prevEl: ".swiper-navigation-prev"
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation, Pagination]}
+        className='h-[500px]'
+      >
+        {testimonialData.map((person, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+                <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                  <div className='flex flex-col justify-center text-center'>
+                    <div className='mb-2 mx-auto'>
+                      <Image
+                        src={person.image}
+                        width={100}
+                        height={100}
+                        alt='testimonial image'
+                      />
+                    </div>
+                    <div className='text-lg'>{person.name}</div>
+                    <div className='text-[12px] uppercase text-p tracking-widest'>
+                      {person.position}
+                    </div>
                   </div>
-                  <div className='text-lg'>{person.name}</div>
-                  <div className='text-[12px] uppercase text-p tracking-widest'>
-                    {person.position}
+                </div>
+                <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+                  <div className='mb-4'>
+                    <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
+                  </div>
+                  <div className='xl:text-lg text-center md:text-left'>
+                    {person.message}
                   </div>
                 </div>
               </div>
-              <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
-                <div className='mb-4'>
-                  <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
-                </div>
-                <div className='xl:text-lg text-center md:text-left'>
-                  {person.message}
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+        <div className="flex text-5xl cursor-pointer">
+          <SlArrowLeft className="swiper-navigation-prev absolute -left-[6%] top-[61%] md:top-[34%] text-accent" />
+          <SlArrowRight type="button" className="swiper-navigation-next absolute -right-[6%] top-[61%] md:top-[34%] text-accent" />
+        </div>
+    </div>
   );
 };
 
