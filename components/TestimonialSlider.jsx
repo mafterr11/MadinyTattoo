7305b254e8e461397615e124b525387d6bbaa6b0
Alexandru Maftei <mafterr11@gmail.com>
2023-12-07 13:33:59 +0200

    Commit

--- components/TestimonialSlider.jsx
+++ components/TestimonialSlider.jsx
@@ -4,21 +4,21 @@ const testimonialData = [
   {
     image: "/t-avt-1.png",
     name: "Ana Maria",
-    position: "Client Tatuaj",
+    position: "Tattoo Customer",
     message:
       "Sincer, nu o să mai merg în altă parte VREODATĂ să mă tatuez. Sunt atât de superbe și a fost atât de faină atmosfera la tine, încât aș simți că te-aș trăda dacă aș merge în altă parte. Abia aștept să revin!",
   },
   {
     image: "/t-avt-2.png",
     name: "Ioana",
-    position: "Client Tatuaj",
+    position: "Tattoo Customer",
     message:
       "Tatuajul este absolut divin. Îți mulțumesc din suflet, ești cea mai profesionistă și pricepută de pe piață. Nu aș fi avut încredere în altcineva pentru primul tatuaj!",
   },
   {
     image: "/t-avt-3.png",
     name: "Andreea",
-    position: "Client Tatuaj",
+    position: "Tattoo Customer",
     message:
       "Nu pot să nu admir cât de incredibil este tatuajul meu! Mulțumesc din inimă, ești pur și simplu remarcabilă în ceea ce faci!",
   },
@@ -34,8 +34,6 @@ import "swiper/css/pagination";
 
 // Icons
 import { FaQuoteLeft } from "react-icons/fa";
-import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
-
 
 // Required Modules
 import { Navigation, Pagination } from "swiper";
@@ -43,56 +41,47 @@ import Image from "next/image";
 
 const TestimonialSlider = () => {
   return (
-    <div className="relative">
-      <Swiper
-        navigation={{
-          nextEl: ".swiper-navigation-next",
-          prevEl: ".swiper-navigation-prev"
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Navigation, Pagination]}
-        className='h-[500px] lg:h-[300px]'
-      >
-        {testimonialData.map((person, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
-                <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
-                  <div className='flex flex-col justify-center text-center'>
-                    <div className='mb-2 mx-auto'>
-                      <Image
-                        src={person.image}
-                        width={100}
-                        height={100}
-                        alt='testimonial image'
-                      />
-                    </div>
-                    <div className='text-lg'>{person.name}</div>
-                    <div className='text-[12px] uppercase text-p tracking-widest'>
-                      {person.position}
-                    </div>
+    <Swiper
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+      className='h-[500px] lg:h-[300px]'
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+              <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                <div className='flex flex-col justify-center text-center'>
+                  <div className='mb-2 mx-auto'>
+                    <Image
+                      src={person.image}
+                      width={100}
+                      height={100}
+                      alt='testimonial image'
+                    />
                   </div>
-                </div>
-                <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
-                  <div className='mb-4'>
-                    <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
-                  </div>
-                  <div className='xl:text-lg text-center md:text-left'>
-                    {person.message}
+                  <div className='text-lg'>{person.name}</div>
+                  <div className='text-[14px] uppercase text-accent font-semibold tracking-widest'>
+                    {person.position}
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-        <div className="flex text-5xl cursor-pointer">
-          <SlArrowLeft className="swiper-navigation-prev absolute -left-[6%] top-[61%] md:top-[34%] text-accent" />
-          <SlArrowRight type="button" className="swiper-navigation-next absolute -right-[6%] top-[61%] md:top-[34%] text-accent" />
-        </div>
-    </div>
+              <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+                <div className='mb-4'>
+                  <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
+                </div>
+                <div className='xl:text-lg text-center md:text-left'>
+                  {person.message}
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
--- components/pages/TestimonialePage.jsx
+++ components/pages/TestimonialePage.jsx
@@ -6,14 +6,14 @@ import { fadeIn } from "../../variants";
 const TestimonialePage = () => {
   return (
     <div className='h-full py-24 xs:py-0 text-center'>
-      <div className='container mx-auto h-screen flex flex-col justify-center'>
+      <div className='container mx-auto h-screen flex flex-col justify-center lg:gap-y-48'>
         {/* title */}
         <motion.h2
           variants={fadeIn("up", 0, 2)}
           initial='hidden'
           animate='show'
           exit='hidden'
-          className='h2 mb-8 xl:mb-24'
+          className='h2 mb-8 xl:mb-0'
         >
           Părerea <span className='text-accent'>clienților.</span>
         </motion.h2>
