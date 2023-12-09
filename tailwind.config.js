5d91151b91e20507993bd7edf9e0cd48c95b2fbb
mafterr11 <mafterr11@gmail.com>
2023-12-09 21:58:09 +0200

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,9 @@ module.exports = {
       },
       backgroundImage: {
         site: "url('/bgMada2.jpg')",
-        tatuaje: "url('/bgMada.jpg')",
+        micropigmentare: "url('/backgrounds/micropigmentare.jpg')",
+        micropigmentare2: "url('/backgrounds/micropigmentare2.jpg')",
+        despre: "url('/backgrounds/despre.jpg')",
       },
       animation: {
         "spin-slow": "spin 6s linear infinite",
