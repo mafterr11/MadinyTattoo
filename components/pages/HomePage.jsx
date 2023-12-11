fb1f99bc18dba5713cc817a5e7b33ddef0e5ef71
Alexandru Maftei <mafterr11@gmail.com>
2023-12-11 13:49:32 +0200

    Commit

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -1,10 +1,7 @@
-// "use client";
 // Components
 import ProjectsBtn from "../ProjectsBtn";
 
 // Framer
-// import { motion } from "framer-motion";
-// import { fadeIn } from "../../variants";
 
 const HomePage = () => {
   return (
