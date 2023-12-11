d977404b30b2370e43179f83698e818f30023e43
Alexandru Maftei <mafterr11@gmail.com>
2023-12-11 10:33:59 +0200

    Commit

--- app/(micropigmentare)/layout.jsx
+++ app/(micropigmentare)/layout.jsx
@@ -2,5 +2,5 @@ import "../globals.css";
 import Layout from "../../components/Layout";
 
 export default function RootLayout({ children }) {
-  return <Layout backgroundClass="bg-micropigmentare2 md:bg-micropigmentare bg-cover bg-no-repeat bg-scroll lg:bg-fixed bg-center relative">{children}</Layout>;
+  return <Layout backgroundClass="bg-micropigmentare2 md:bg-micropigmentare bg-cover bg-no-repeat bg-fixed bg-center relative">{children}</Layout>;
 }
--- app/(tatuaje)/layout.jsx
+++ app/(tatuaje)/layout.jsx
@@ -2,5 +2,5 @@ import "../globals.css";
 import Layout from "../../components/Layout";
 
 export default function RootLayout({ children }) {
-  return <Layout backgroundClass="bg-tatuaje bg-cover  bg-no-repeat bg-scroll lg:bg-fixed bg-center relative">{children}</Layout>;
+  return <Layout backgroundClass="bg-tatuaje bg-cover  bg-no-repeat bg-fixed bg-center relative">{children}</Layout>;
 }
