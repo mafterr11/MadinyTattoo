53a1af5f1a01f557a5be8d0a64e05321b98920c2
Alexandru Maftei <mafterr11@gmail.com>
2023-12-11 13:36:53 +0200

    Commit

--- variants.jsx
+++ variants.jsx
@@ -1,12 +1,12 @@
 export const fadeIn = (direction, delay) => {
   return {
     hidden: {
-      y: direction === 'up' ? 30 : direction === 'down' ? -30 : 0,  //Din 80 in 30 aici
+      y: direction === 'up' ? 80 : direction === 'down' ? -80 : 0,
       opacity: 0,
-      x: direction === 'left' ? 30 : direction === 'right' ? -30 : 0,  //Din 80 in 30 aici
+      x: direction === 'left' ? 80 : direction === 'right' ? -80 : 0,
       transition: {
         type: 'tween',
-        duration: 0.8, // Reduced duration for faster animation 1.6
+        duration: 1.5,
         delay: delay,
         ease: [0.25, 0.6, 0.3, 0.8],
       },
@@ -17,7 +17,7 @@ export const fadeIn = (direction, delay) => {
       opacity: 1,
       transition: {
         type: 'tween',
-        duration: 0.7, // Reduced duration for faster animation 1.4
+        duration: 1.4,
         delay: delay,
         ease: [0.25, 0.25, 0.25, 0.75],
       },
