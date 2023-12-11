4e5e18391ae27c8eaa8e65f282405b2824af896c
Alexandru Maftei <mafterr11@gmail.com>
2023-12-11 13:53:39 +0200

    Commit

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-"use client";
+// "use client";
 // Components
 import ProjectsBtn from "../ProjectsBtn";
 
 // Framer
-import { motion } from "framer-motion";
-import { fadeIn } from "../../variants";
+// import { motion } from "framer-motion";
+// import { fadeIn } from "../../variants";
 
 const HomePage = () => {
   return (
@@ -13,41 +13,41 @@ const HomePage = () => {
       <div className='w-full h-screen py-12 xs:py-0'>
         <div className='container mx-auto flex flex-col justify-center text-center xl:text-left xl:pt-40 h-screen xl:h-full '>
           {/* title */}
-          <motion.h1
-            variants={fadeIn("right", 0.2)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+          <h1
+            // variants={fadeIn("right", 0.2)}
+            // initial='hidden'
+            // animate='show'
+            // exit='hidden'
             className='h1'
           >
             Transformăm Ideile <br /> In{" "}
             <span className='text-accent '>Realitate</span>
-          </motion.h1>
+          </h1>
           {/* subtitle */}
-          <motion.p
-            variants={fadeIn("left", 0.4)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+          <p
+            // variants={fadeIn("left", 0.4)}
+            // initial='hidden'
+            // animate='show'
+            // exit='hidden'
             className='max-w-sm md:max-w-2xl mx-auto xl:mx-0 mb-10 xl:mb-16 xl:text-lg text-p text-xl'
           >
             În spațiul nostru creativ, transformăm fiecare concept într-o
             realitate vibrantă și autentică. Fiecare design captează unicitatea
             ta, conturând și detaliind povestea personală.
-          </motion.p>
+          </p>
           {/* btn */}
           <div className='flex justify-center xl:hidden relative'>
             <ProjectsBtn />
           </div>
-          <motion.div
-            variants={fadeIn("right", 0.5)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+          <div
+            // variants={fadeIn("right", 0.5)}
+            // initial='hidden'
+            // animate='show'
+            // exit='hidden'
             className='hidden xl:flex'
           >
             <ProjectsBtn />
-          </motion.div>
+          </div>
         </div>
       </div>
     </div>
