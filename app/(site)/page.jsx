398bc8215b63d5b0b9b42d51c30d58310ab9a5e6
mafterr11 <mafterr11@gmail.com>
2023-12-13 21:08:46 +0200

    Commit

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,8 +1,12 @@
 import HomePage from "../../components/pages/HomePage";
 
 export const metadata = {
+  title: "Madiny Tattoo",
   description:
-    "Bine ați venit la Madiny Tattoo, locul unde arta devine permanentă. Descoperă cele mai recente creații în tatuaje, micropigmentare și eliminare eficientă a tatuajelor. Expertiză, pasiune și creație autentică.",
+    "Bine ai venit la Madiny Tattoo, unde arta se întâlnește cu pielea. Descoperă designuri unice de tatuaje și lucrări excepționale realizate de artiștii noștri talentați. Explorează o lume a creativității și a expresiei personale cu portofoliul nostru diversificat.",
+
+  keywords:
+    "artist tatuaj romania, tatuator talentat, studio tatuaje personalizate, arta profesionala de tatuaj, tatuaje detaliat executate, calitate premium tatuaje, micropigmentare profesionala, tehnici avansate micropigmentare, proceduri estetice micropigmentare, tehnologii laser tatuaje, eliminare eficienta tatuaje",
 };
 
 const Acasa = () => {
