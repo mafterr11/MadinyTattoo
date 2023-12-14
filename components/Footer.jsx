fbedc773c83e2a72a95738486769810d754999c2
mafterr11 <mafterr11@gmail.com>
2023-12-15 00:11:38 +0200

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,16 +3,19 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer>
-      <div className='container px-5 py-8 mx-auto flex items-center justify-evenly sm:flex-row flex-col pb-24 xl:pb-4 relative z-[2]'>
+      <div className='container px-5 py-8 mx-auto flex items-center justify-center sm:flex-row flex-col pb-24 xl:pb-4 md:gap-x-6 relative z-[2]'>
         <div className="md:text-xl lg:text-2xl xl:text-base">
-          Made by{" "}
+        Creat de {" "}
           <Link
             href='https://www.linkedin.com/in/maftei-alexandru/'
             target='blank'
             className='hover'
           >
-            <span className='text-accent hover:text-white'>Maftei</span>
+            <span className='text-accent hover:text-white'>Maftei Alexandru</span>
           </Link>
+        </div>
+        <div>
+          <p>© All Rights Reserved</p>
         </div>
       </div>
     </footer>
