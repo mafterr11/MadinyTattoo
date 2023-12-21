0387f1d68c427788a744265875addc685d8a35db
Alexandru Maftei <mafterr11@gmail.com>
2023-12-21 13:33:29 +0200

    Commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row justify-between items-center py-6'>
           {/* Logo */}
-          <h3 className='h3 lg:pt-4'>
+          <h3 className='h3 flex lg:pt-4'>
             MADINY.<span className='text-accent'>TATTOO</span>
           </h3>
           <div>
