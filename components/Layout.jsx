8b503faddef30005b7d8b26adb8e23c12bfcab03
mafterr11 <mafterr11@gmail.com>
2023-12-26 01:31:10 +0200

    c

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { Poppins } from "next/font/google";
-import Head from 'next/head';
 // Component
 import Nav from "./Nav";
 import Header from "./Header";
@@ -15,7 +14,7 @@ const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   subsets: ["latin"],
   variable: "--font-poppins",
-  display: "swap",
+  display: "swap"
 });
 
 const Layout = ({ children, backgroundClass }) => {
@@ -27,24 +26,11 @@ const Layout = ({ children, backgroundClass }) => {
     >
       <AnimatePresence mode='wait'>
         <body className={`${poppins.className} ${backgroundClass} `}>
-          <motion.div key={router} className='page bg-primary/40'>
+          <motion.div key={router} className="page bg-primary/40">
             <Transition />
             <Nav />
             <Header />
-            <main>
-              <Head>
-                <meta property='og:image' content='/opengraph-image.png' />
-                <meta
-                  property='og:image:secure_url'
-                  content='https://madinytattoo.ro/opengraph-image.png'
-                />
-                <meta property='og:image:type' content='image/png' />
-                <meta property='og:image:width' content='751' />
-                <meta property='og:image:height' content='500' />
-                {/* Additional OG tags as needed */}
-              </Head>
-              {children}
-            </main>
+            {children}
             <Footer />
           </motion.div>
         </body>
