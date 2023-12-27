1107ee5440cbe7bc0ce2c71d2d26b178d8d1d3ef
mafterr11 <mafterr11@gmail.com>
2023-12-27 13:02:26 +0200

    .

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -6,7 +6,7 @@ export const metadata = {
     "Bine ai venit la Madiny Tattoo, unde arta se întâlnește cu pielea. Descoperă designuri unice de tatuaje și lucrări excepționale realizate de artiștii noștri talentați. Explorează o lume a creativității și a expresiei personale cu portofoliul nostru diversificat.",
 
   keywords:
-    "artist tatuaj romania, tatuator talentat, studio tatuaje personalizate, arta profesionala de tatuaj, tatuaje detaliat executate, calitate premium tatuaje, micropigmentare profesionala, tehnici avansate micropigmentare, proceduri estetice micropigmentare, tehnologii laser tatuaje, eliminare eficienta tatuaje",
+    "artist tatuaj romania, tatuator talentat, studio tatuaje personalizate, arta profesionala de tatuaj, tatuaje detaliat executate, calitate premium tatuaje, micropigmentare profesionala, tehnici avansate micropigmentare, proceduri estetice micropigmentare, tehnologii laser tatuaje, eliminare eficienta tatuaje, tatuaje bucuresti, tatuaje pipera, salon tatuaj bucuresti, salon tatuaj",
 };
 
 const Acasa = () => {
