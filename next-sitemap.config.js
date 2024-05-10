b7f13a7a4a5be1d0c17b4e33e70120c848999b3d
mafterr11 <mafterr11@gmail.com>
2024-05-10 17:44:55 +0300

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.madinytattoo.ro' || "http://localhost:3000",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
