6977898c1a527c8a48ce97b8d350599848bc063b
mafterr11 <mafterr11@gmail.com>
2024-05-15 22:49:01 +0300

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,41 +1,15 @@
-"use client";
-import { Poppins } from "next/font/google";
+import LayoutContent from "./LayoutContent";
 import "./globals.css";
-// Component
-import Nav from "../components/Nav";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Transition from "../components/Transition";
-// Router
-import { usePathname } from "next/navigation";
-// Framer
-import { AnimatePresence, motion } from "framer-motion";
-
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  subsets: ["latin"],
-  variable: "--font-poppins",
-  display: "swap",
-});
+import { constructMetadata } from "../lib/utils";
+export const metadata = constructMetadata();
 
 export default function RootLayout({ children }) {
-  const currentRoute = usePathname();
   return (
     <html
       lang='ro'
       className='bg-site bg-cover bg-no-repeat bg-scroll lg:bg-fixed bg-center relative'
     >
-      <AnimatePresence mode='wait'>
-        <body className={`${poppins.className}`}>
-          <motion.div key={currentRoute} className='page bg-primary/40'>
-            <Transition />
-            <Nav />
-            <Header />
-            {children}
-            {currentRoute === "/" ? <Footer /> : currentRoute === "/contact" ? <Footer /> : currentRoute === "/testimoniale" ? <Footer /> : currentRoute === "/proiecte" ? <Footer /> : null} 
-          </motion.div>
-        </body>
-      </AnimatePresence>
+      <LayoutContent children={children} />
     </html>
   );
-};
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
 import HomePage from "../components/pages/HomePage";
-import { constructMetadata } from "../lib/utils";
 
-export const metadata = constructMetadata();
 
 const Acasa = () => {
   return <HomePage />;
