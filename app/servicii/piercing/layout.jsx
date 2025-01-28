ad7afc27ee71c68039f8b12e6a3fb7fbbf3ac25d
mafterr11 <mafterr11@gmail.com>
2025-01-28 19:38:14 +0200

    .

--- app/servicii/piercing/layout.jsx
+++ app/servicii/piercing/layout.jsx
@@ -1,7 +1,7 @@
 import Footer from "../../../components/layout/Footer";
 export default function PiercingLayout({ children }) {
   return (
-    <div className='bg-micropigmentare bg-cover bg-no-repeat bg-fixed bg-center relative'>
+    <div className='bg-piercing bg-cover bg-no-repeat bg-fixed bg-center xl:bg-[center_top_-20rem] relative'>
       <div className='page bg-primary/40'>
         {children}
         <Footer />
