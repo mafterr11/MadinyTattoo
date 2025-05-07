3bc0ec8349a863e870dea89b3a7ba97938d6c9e8
Alexandru Maftei <mafterr11@gmail.com>
2025-05-07 13:52:58 +0300

    .

---
+++ components/pages/NewGallery.jsx
@@ -0,0 +1,120 @@
+import Image from "next/image";
+import React from "react";
+
+const images = [
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo1.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo2.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo3.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo4.webp",
+  },
+
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo5.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo6.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo7.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo8.webp",
+  },
+
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo9.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo10.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo11.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo12.webp",
+  },
+
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo13.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo14.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo15.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo16.webp",
+  },
+
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo17.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo18.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo19.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo20.webp",
+  },
+
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo21.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo22.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo23.webp",
+  },
+  {
+    title: "Poza tatuaje",
+    path: "/gallery/tattoo24.webp",
+  },
+];
+
+const NewGallery = () => {
+  return (
+    <div className="custom-scrollbar grid h-[100vh] max-xl:w-[95vw] grid-cols-1 gap-4 overflow-auto md:grid-cols-3 xl:min-w-[1240px]">
+      {images.map((image, index) => (
+        <div key={index} className="relative aspect-square">
+          <Image src={image.path} fill alt={image.title} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NewGallery;
--- components/pages/ProiectePage.jsx
+++ components/pages/ProiectePage.jsx
@@ -1,29 +1,26 @@
 // "use client";
 import WorkSlider from "../WorkSlider";
+import NewGallery from "./NewGallery";
 
 const ProiectePage = () => {
   return (
-    <div className='flex items-center'>
-      <div className='xl:container mx-auto h-full py-36 xl:py-60'>
-        <div className='flex xl:h-full flex-col xl:flex-row gap-x-8'>
-          {/* Text */}
-          <div className='text-center flex xl:w-[30vw] flex-col xl:text-left mb-4 xl:mb-0'>
-            <h1 className='xl:mt-8'>
-              <span className='text-accent'>Lucrările</span> mele.
-            </h1>
-            <p className='mb-4 xl:max-w-[500px] mx-auto lg:mx-0'>
-              Prin arta mea, transform viziunile în realitate, aducând emoție și
-              semnificație fiecărui tatuaj. Explorează portofoliul meu și
-              lasă-te captivat de poveștile vizuale pe care le-am imprimat pe
-              piele.
-            </p>
-          </div>
-          {/* Slider */}
-          <div className='w-full xl:max-w-[45%]'>
-            <WorkSlider />
-          </div>
-        </div>
+    <div className="flex h-full flex-col items-center justify-center gap-x-8 gap-y-32 py-36 xl:h-full xl:py-60">
+      {/* Text */}
+      <div className="mb-4 flex flex-col text-center xl:mb-0 xl:w-[50vw]">
+        <h1 className="xl:mt-8">
+          <span className="text-accent">Lucrările</span> mele.
+        </h1>
+        <p className="mb-4 lg:mx-0">
+          Prin arta mea, transform viziunile în realitate, aducând emoție și
+          semnificație fiecărui tatuaj. Explorează portofoliul meu și lasă-te
+          captivat de poveștile vizuale pe care le-am imprimat pe piele.
+        </p>
       </div>
+      {/* Slider */}
+       
+        {/* <WorkSlider /> */}
+        <NewGallery />
+      
     </div>
   );
 };
